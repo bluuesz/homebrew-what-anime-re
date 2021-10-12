81757499d9c5a3d0a280bee908f9e7242e464ee2
Wosley Vacemberg <wosleyv@gmail.com>
2021-10-12 00:51:32 -0300

    fix url

--- Formula/what-anime-re.rb
+++ Formula/what-anime-re.rb
@@ -4,7 +4,7 @@
 class WhatAnimeRe < Formula
   desc "A CLI to find anime scene from trace.moe"
   homepage "https://github.com/bluuesz/what-anime-reasonml"
-  url "https://github.com/bluuesz/what-anime-reasonml/releases/download/v0.1.0/what-anime-re-homebrew.tar.gz"
+  url "https://github.com/bluuesz/what-anime-reasonml/releases/download/v0.1.1/what-anime-re-homebrew.tar.gz"
   sha256 "716516a85149eac9398c99d9fb80d835b9aa93d95083ebbed0f82ecb9662d007"
   version "0.1.1"
 
