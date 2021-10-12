c03a7d932a3209e3e25e7f09888e3124779884e6
Wosley Vacemberg <wosleyv@gmail.com>
2021-10-12 00:38:42 -0300

    update sha256

--- Formula/what-anime-re.rb
+++ Formula/what-anime-re.rb
@@ -5,7 +5,7 @@ class WhatAnimeRe < Formula
   desc "A CLI to find anime scene from trace.moe"
   homepage "https://github.com/bluuesz/what-anime-reasonml"
   url "https://github.com/bluuesz/what-anime-reasonml/releases/download/v0.1.0/what-anime-re-homebrew.tar.gz"
-  sha256 "c8b1c2f1013daab782e7b510d92ccc7ec7e95666d6380f63a6be3cea7970cf8c"
+  sha256 "716516a85149eac9398c99d9fb80d835b9aa93d95083ebbed0f82ecb9662d007"
   version "0.1.0"
 
   def install
